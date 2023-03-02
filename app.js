478eb23bccc9cae20d1edba53f76614a76333010
Daniel O Sousa <oliveira.daaaaniel@gmail.com>
2023-03-02 14:26:14 -0300

    Stablishing connection to db

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 
+const db = require("./src/db");
+
 const port = process.env.PORT || 3000;
 const app = express();
 
@@ -12,4 +14,15 @@ app.get('/', (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => console.log(`App listening on port ${port}`));
+async function start() {
+  try {
+    await db.main();
+
+    console.log("Successfully connected to DB");
+    console.log(`App listening on port ${port}`)
+  } catch(err) {
+    console.log(`Not connected to DB. Error: ${err}`);
+  }
+}
+
+app.listen(port, start);
