fc1ccbbd886d5d9a8416003fab2e53336cd5c441
Daniel O Sousa <oliveira.daaaaniel@gmail.com>
2023-03-06 14:27:00 -0300

    Adding post creation

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -1,5 +1,20 @@
-async function create(req, res) {
-  res.send("Post created!");
+const Post = require("../models").Post;
+
+async function create(req, res, next) {
+  try {
+    const { id: author } = req.user;
+    const { title, content } = req.body;
+
+    const post = await Post.create({
+      title,
+      content,
+      author
+    });
+
+    res.status(201).json({ post });
+  } catch(err) {
+    next(err);
+  }
 }
 
 async function read(req, res) {
--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,12 +1,14 @@
 const express = require("express");
 
+const auth = require("../middleware").auth;
+
 const post = require("../controllers").post;
 
 const router = express.Router();
 
 router.get('/', post.read);
 router.get("/:id", post.readOne);
-router.post('/', post.create);
+router.post('/', auth, post.create);
 router.put("/:id", post.update);
 router.delete("/:id", post.destroy);
 
